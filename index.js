893e3d0856b9f343370d9fc174e841a2ec5665d4
yadavdotcodes <adi@mail.io>
2022-09-13 11:33:33 +0530

    Initial Commit!

---
+++ index.js
@@ -0,0 +1,32 @@
+const fetch = require("cross-fetch");
+
+class ursh {
+   /**
+    * Shorten A URL!
+    */
+   static add = async (url, custom) => {
+      let response = await fetch("https://baby.deta.dev/create", {
+         method: "POST",
+         body: JSON.stringify({ url, custom }),
+         headers: {
+            "Content-Type": "application/json"
+         }
+      });
+      return await response.json();
+   }
+   /**
+   * Get Details Of Shorten URL!
+   */
+   static get = async (code) => {
+      let response = await fetch("https://baby.deta.dev/data", {
+         method: "POST",
+         body: JSON.stringify({ code }),
+         headers: {
+            "Content-Type": "application/json"
+         }
+      });
+      return await response.json();
+   }
+}
+
+module.exports = ursh;
